2a9637dbcb6f52e82f901ac6e888b351f0254d51
Georgina <georgina.armstrong@gmail.com>
2015-06-09 17:19:22 +0200

    Still working

--- pollutantmean v2.R
+++ pollutantmean v2.R
@@ -4,8 +4,7 @@ setwd("C:/Users/georginaa/Documents/R/datasciencecoursera")
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
 
-  data_vector <- numeric()          ## creating empty numeric vector to hold all the means
-  slot <- 1                         ## starting slot, always 1 (in case ID doesn't start at 1)
+  data_vector <- numeric()          ## creating empty numeric vector to hold all the datapoints
   
   files_list <- list.files(path = "C:/Users/georginaa/Documents/R/datasciencecoursera/specdata", all.files = FALSE,
              full.names = TRUE, include.dirs = TRUE)  ## creating a vector of all file paths so that they can be read
@@ -14,7 +13,6 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     
     quick_df <- data.frame(read.csv(files_list[i], header = TRUE))   ## starting the for loop, to go through ID files, and read them
     quick_vector <- quick_df[,pollutant]
-    
     data_vector <- rbind(data_vector, quick_vector)
   
   }
@@ -34,3 +32,5 @@ pollutantmean("specdata", "sulfate", 2:3)
 pollutantmean("specdata", "sulfate", 1:10)  ## Should give 4.064 -- INCORRECT
 pollutantmean("specdata", "nitrate", 70:72) ## Should give 1.706 -- INCORRECT
 pollutantmean("specdata", "nitrate", 23)    ## Should give 1.281 -- CORRECT
+
+
